80018fd40f3ed2d17a4b6fd38646f8de45854d49
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-01-20 22:45:13 +0530

    Added responsiveness to blog container skeleton

--- src/components/BlogContainerSkeleton.js
+++ src/components/BlogContainerSkeleton.js
@@ -4,11 +4,14 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 function BlogContainerSkeleton() {
     return (
-        <div className="max-w-2xl w-1/5 mx-auto my-5 p-4 border border-grey rounded-md shadow-lg bg-black">
-            <Skeleton height={30} width={`80%`} enableAnimation={true} baseColor={"#808080"} />
-            <Skeleton height={20} width={`60%`} enableAnimation={true} className="my-2" baseColor={"#808080"} /> {/* Date */}
+        <div className="max-w-2xl w-10/12 md:w-1/2 xl:w-1/3  mx-auto my-5 p-4 border border-grey rounded-md shadow-lg bg-black" style={{ textAlign: 'center' }}>
+            <div style={{ display: 'inline-block', width: '100%' }}>
+                <Skeleton height={30} width={`80%`} enableAnimation={true} baseColor={"#808080"} />
+            </div>
+            <div style={{ display: 'inline-block', width: '100%', marginTop: '8px' }}>
+                <Skeleton height={20} width={`60%`} enableAnimation={true} baseColor={"#808080"} />
+            </div>
         </div>
-    )
+    );
 }
-
 export default BlogContainerSkeleton;

f295787030aa26e71bfc852a6a33a9279573f7f0
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-01-21 01:20:10 +0530

    Adjusted responsiveness

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,12 +4,10 @@ import Footer from "./Footer";
 
 function Layout({ children }) {
     return (
-        <div className="bg-black">
-        <div className="bg-black mx-auto max-w-screen-2xl w-screen">
+        <div className="bg-black mx-auto">
         <Navbar />
         { children }
         <Footer />
-        </div>
         </div>
     )
 }

1b9b441736c860a0284d4db06326b47bac9b7f5e
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-01-21 13:11:23 +0530

    Adjusted responsiveness of blog detail page

--- src/pages/BlogDetailPage.js
+++ src/pages/BlogDetailPage.js
@@ -44,11 +44,11 @@ function BlogDetailPage() {
             : 
             <>
             <div className="flex flex-col items-center justify-center p-5">
-            <h1 className="text-white text-4xl font-bold">{title}</h1>
+            <h1 className="text-white text-4xl font-bold text-center">{title}</h1>
             <p className="text-grey text-lg p-3">{formatDate(createdAt)}</p>
             </div>
-            <div className="flex items-center justify-center"> 
-            <div className="text-white blog text-lg pl-10 pr-10 mt-10 leading-8" dangerouslySetInnerHTML={{__html: content}}></div>
+            <div className="flex items-center justify-center p-5"> 
+            <div className="text-white blog text-lg mt-10 leading-8" dangerouslySetInnerHTML={{__html: content}}></div>
             </div>
             </>
         }

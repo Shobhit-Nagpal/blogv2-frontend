c442e11e692eac33e873103235aa62d926e8a7c5
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-01-21 14:15:43 +0530

    Footer links open on new page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ function Footer() {
         <p className="text-grey lg:ml-52">Copyright &#169; 2024</p>
         </div>
         <div className="flex gap-5 justify-between items-center lg:mr-52">
-        <a className="text-grey hover:text-white text-2xl" href="https://github.com/Shobhit-Nagpal"><FaGithub /></a>
-        <a className="text-grey hover:text-white text-2xl" href="https://x.com/shbhtngpl"><FaXTwitter /></a>
-        <a className="text-grey hover:text-white text-2xl" href="https://linkedin.com/in/shobhitnagpal"><FaLinkedin /></a>
+        <a className="text-grey hover:text-white text-2xl" href="https://github.com/Shobhit-Nagpal" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+        <a className="text-grey hover:text-white text-2xl" href="https://x.com/shbhtngpl" target="_blank" rel="noopener noreferrer"><FaXTwitter /></a>
+        <a className="text-grey hover:text-white text-2xl" href="https://linkedin.com/in/shobhitnagpal" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
         </div>
         </div>
     )

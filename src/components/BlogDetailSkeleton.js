b406925cd976a467c54b7a408d94deb6964e33aa
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-01-21 13:25:49 +0530

    Adjusted responsiveness of blog detail skeleton

--- src/components/BlogDetailSkeleton.js
+++ src/components/BlogDetailSkeleton.js
@@ -6,9 +6,9 @@ function BlogDetailSkeleton() {
     return (
         <SkeletonTheme baseColor="#202020" highlightColor="#444">
             <div className="max-w-4xl w-10/12 md:w-1/2 xl:w-1/3 mx-auto my-5 p-4 rounded-md shadow-lg bg-black">
-                    <Skeleton height={40} width="100%" enableAnimation={true} />
+                    <Skeleton height={60} width="100%" enableAnimation={true} />
 
-                <div className="space-y-4 mt-10">
+                <div className="space-y-4 mt-32">
                     {Array(30).fill().map((_, idx) => (
                         <Skeleton key={idx} height={20} width="100%" enableAnimation={true} />
                     ))}
